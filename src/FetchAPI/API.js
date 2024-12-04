ec00666be80d1ae005930768dda278248d0a69c4
Conner Krenzer <krenzer84@gmail.com>
2024-12-04 15:02:17 -0500

    token

--- src/FetchAPI/API.js
+++ src/FetchAPI/API.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API_BASE_URL = "https://the-one-api.dev/v2";
-const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+const TOLKIEN = import.meta.env.TOLKIEN;
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
-    Authorization: `Bearer ${API_TOKEN}`,
+    Authorization: `Bearer ${TOLKIEN}`,
   },
 });
